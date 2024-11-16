6f712ae731c47905e7fc46322e3405a457329ada
Preet <preetpatel038@gmail.com>
2024-11-15 18:51:48 -0800

    added all the remaining tables

---
+++ Recipe_Ingredient_Junction.cs
@@ -0,0 +1,11 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+public class Recipe_Ingredient_Junction
+    {
+    [Key]
+    public int Recipe_id { get; set; }
+    public int Ingredient_id { get; set; }
+    public int Quantity_required { get; set; }
+    }
+
---
+++ User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+    public class User
+    {
+        [Key]
+       public int User_id { get; set; }
+        public string User_name { get; set; } = string.Empty;
+        public string First_name { get; set; } = string.Empty;
+        public string Last_name { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        
+
+    }
+
